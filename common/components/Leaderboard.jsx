ae0aaef32fe0a71e9a0f096bd133615597b476cf
jeffreywescott <jeffrey@learnersguild.org>
2016-02-03 11:45:02 -0800

    render leaderboard

--- common/components/Leaderboard.jsx
+++ common/components/Leaderboard.jsx
@@ -3,11 +3,31 @@ import React, { Component, PropTypes } from 'react'
 export default class Leaderboard extends Component {
   render() {
     const { leaderboard } = this.props
-    console.log('leaderboard:', leaderboard)
-    
-    return (
-      <div className="display-3">TODO: implement leaderboard</div>
-    )
+
+    if (leaderboard.isLoading) {
+      return (
+        <div className="display-3">
+          Loading ...
+          <br />
+          <i className="fa fa-spinner fa-spin"></i>
+        </div>
+      )
+    } else {
+      const mediaElements = leaderboard.leaders.map((leader, i) => {
+        return (
+          <div key={i} className="media">
+            <a className="media-left" href={`https://github.com/${leader.login}`}>
+              <img className="media-object" width="64" height="64" src={leader.avatar_url} alt={leader.login} />
+            </a>
+            <div className="media-body">
+              <h4 className="media-heading">{leader.login}</h4>
+              <p># of commits: {leader.count}</p>
+            </div>
+          </div>
+        )
+      })
+      return <div>{mediaElements}</div>
+    }
   }
 }
 
